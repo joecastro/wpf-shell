104c44556f50a3b77ae10d43c6a0cac6b2dd2fac
Joe Castro <joe.castro@snapchat.com>
2015-08-10 16:00:00 -0700

    Inlining standard as a subproject

---
+++ Microsoft.Windows.Shell/standard.net/Csv.cs
@@ -0,0 +1,168 @@
+﻿namespace Standard
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    internal static class Csv
+    {
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        public static string Escape(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return "";
+            }
+
+            if (str.Contains("\""))
+            {
+                str = str.Replace("\"", "\"\"");
+            }
+
+            if (str.IndexOfAny(new char[] { ',', '"', '\n' }) > -1)
+            {
+                str = '\"' + str + '\"';
+            }
+
+            return str;
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        public static string MakeLine(string[] data)
+        {
+            var sb = new StringBuilder();
+            foreach (var cell in data)
+            {
+                sb.Append(Escape(cell));
+                sb.Append(',');
+            }
+
+            return sb.ToString(0, sb.Length - 1);
+        }
+
+        // Read a logical line from the stream.  It may contain embedded newlines if it was CSV-style escaped.
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        private static string _ReadLine(StreamReader reader)
+        {
+            int i = 0;
+            bool inEscape = false;
+            bool lastChance = false;
+
+            var sb = new StringBuilder(reader.ReadLine());
+            
+            while (true)
+            {
+                for (; i < sb.Length; ++i)
+                {
+                    if (sb[i] == '\"')
+                    {
+                        inEscape = !inEscape; 
+                    }
+                }
+                if (!inEscape)
+                {
+                    return sb.ToString();
+                }
+                sb.Append("\n");
+                if (lastChance)
+                {
+                    throw new ArgumentException("Invalid CSV data.");
+                }
+                sb.Append(reader.ReadLine());
+                lastChance = reader.EndOfStream;
+            }
+        }
+
+        private static readonly string bellString = Encoding.ASCII.GetString(new byte[] { 7 });
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        private static List<string> _ParseLine(string row)
+        {
+            if (!row.Contains('\"'))
+            {
+                return new List<string>(row.Split(','));
+            }
+
+            // Make it simpler to not have to look for escaped quotes.
+            row = row.Replace("\"\"", bellString);
+
+            var ret = new List<string>();
+            int startIndex = 0;
+            for (int i = 0; i < row.Length; ++i)
+            {
+                if (row[i] == '\"')
+                {
+                    // Skip the opening quote
+                    startIndex = i + 1;
+                    while (row[++i] != '\"')
+                    { }
+
+                    // Remove the trailing quote, and replace back any embedded quotes.
+                    ret.Add(row.Substring(startIndex, i - startIndex).Replace((char)7, '\"'));
+
+                    ++i;
+                    if (i < row.Length && row[i] != ',')
+                    {
+                        throw new ArgumentException("Malformed data.");
+                    }
+                    startIndex = i + 1;
+                }
+                else
+                {
+                    if (i >= row.Length - 1 || row[i] == ',')
+                    {
+                        ret.Add("");
+                        startIndex = i + 1;
+                        continue;
+                    }
+
+                    while (i < row.Length-1 && row[++i] != ',')
+                    { }
+
+                    ret.Add(row.Substring(startIndex, i - startIndex));
+
+                    if (i < row.Length-1 && row[i] != ',')
+                    {
+                        throw new ArgumentException("Malformed data.");
+                    }
+                    startIndex = i + 1;
+                }
+            }
+
+            if (row.EndsWith(",", StringComparison.Ordinal))
+            {
+                ret.Add("");
+            }
+
+            return ret;
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        public static List<Dictionary<string, string>> ReadDocument(StreamReader reader)
+        { 
+            List<string> headers = _ParseLine(_ReadLine(reader));
+            var ret = new List<Dictionary<string, string>>();
+
+            while (!reader.EndOfStream)
+            {
+                var rowData = new Dictionary<string,string>();
+                List<string> cells = _ParseLine(_ReadLine(reader));
+                if (cells.Count != headers.Count)
+                {
+                    throw new ArgumentException("Bad CSV file.");
+                }
+
+                for (int i = 0; i < headers.Count; ++i)
+                {
+                    rowData.Add(headers[i], cells[i]);
+                }
+                ret.Add(rowData);
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ Microsoft.Windows.Shell/standard.net/Wpf/WindowExtensions.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Standard
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Interop;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Attached properties for WPF Windows.
+    /// </summary>
+    internal sealed class WindowExtensions
+    {
+        private IntPtr _hwnd = IntPtr.Zero;
+        private Window _window = null;
+
+        private event Action WindowSourceInitialized;
+
+        private WindowExtensions(Window window)
+        {
+            Assert.IsNotNull(window);
+            _window = window;
+            _hwnd = new WindowInteropHelper(window).Handle;
+
+            if (_hwnd == IntPtr.Zero)
+            {
+                _window.SourceInitialized += _OnWindowSourceInitialized;
+            }
+        }
+
+        private void _OnWindowSourceInitialized(object sender, EventArgs e)
+        {
+            Assert.AreEqual(sender, _window);
+
+            _window.SourceInitialized -= _OnWindowSourceInitialized;
+
+            _hwnd = new WindowInteropHelper(_window).Handle;
+            Assert.IsNotDefault(_hwnd);
+
+            Action handler = WindowSourceInitialized;
+            if (handler != null)
+            {
+                handler();
+            }
+        }
+
+        private static WindowExtensions _EnsureAttachedExtensions(Window window)
+        {
+            Assert.IsNotNull(window);
+
+            var ext = (WindowExtensions)window.GetValue(WindowExtensionsProperty);
+            if (ext == null)
+            {
+                ext = new WindowExtensions(window);
+                window.SetValue(WindowExtensionsProperty, ext);
+            }
+
+            return ext;
+        }
+
+        private static readonly DependencyProperty WindowExtensionsProperty = DependencyProperty.RegisterAttached(
+            "WindowExtensions",
+            typeof(WindowExtensions),
+            typeof(WindowExtensions),
+            new PropertyMetadata(null));
+
+        // Not bothering with CLR attached property getter/setter since this is a private dependency property.
+
+
+        public static readonly DependencyProperty HwndBackgroundBrushProperty = DependencyProperty.RegisterAttached(
+            "HwndBackgroundBrush", 
+            typeof(SolidColorBrush),
+            typeof(WindowExtensions),
+            new PropertyMetadata(
+                Brushes.Pink,
+                (d,e) => _OnHwndBackgroundBrushChanged(d)));
+
+        public static SolidColorBrush GetHwndBackgroundBrush(FrameworkElement window)
+        {
+            Verify.IsNotNull(window, "window");
+            return (SolidColorBrush)window.GetValue(HwndBackgroundBrushProperty);
+        }
+
+        public static void SetHwndBackgroundBrush(FrameworkElement window, SolidColorBrush value)
+        {
+			if (!(window is Window))
+			{
+				return;
+			}
+            Verify.IsNotNull(window, "window");
+            window.SetValue(HwndBackgroundBrushProperty, value);
+        }
+
+        private static void _OnHwndBackgroundBrushChanged(DependencyObject d)
+        {
+            if (DesignerProperties.GetIsInDesignMode(d))
+            {
+                return;
+            }
+
+            var window = d as Window;
+            Verify.IsNotNull(window, "window");
+
+            WindowExtensions ext = _EnsureAttachedExtensions(window);
+
+            if (ext._hwnd == IntPtr.Zero)
+            {
+                ext.WindowSourceInitialized += () => _OnHwndBackgroundBrushChanged(window);
+                return;
+            }
+
+			SolidColorBrush backgroundBrush = (SolidColorBrush)window.GetValue(HwndBackgroundBrushProperty);
+            //if (backgroundBrush == null)
+            //{
+                // Nothing to change.
+            //    return;
+            //}
+
+            Color backgroundColor = backgroundBrush.Color;
+
+            // Not really handling errors here, but they shouldn't matter... Might leak an HBRUSH.
+
+            IntPtr hBrush = NativeMethods.CreateSolidBrush(Utility.RGB(backgroundColor));
+
+            // Note that setting this doesn't necessarily repaint the window right away.
+            // Since the WPF content should cover the HWND background this doesn't matter.
+            // The new background will get repainted when the window is resized.
+            IntPtr hBrushOld = NativeMethods.SetClassLongPtr(ext._hwnd, GCLP.HBRBACKGROUND, hBrush);
+
+            if (IntPtr.Zero != hBrushOld)
+            {
+                NativeMethods.DeleteObject(hBrushOld);
+            }
+        }
+    }
+}
